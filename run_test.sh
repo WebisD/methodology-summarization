8ae10b687443d6ad38ede00dd8943c6aa38cf3b1
WebisD <weverson.webis@gmail.com>
2022-10-29 13:28:12 -0300

    Add scripts for train/test/eval

---
+++ run_test.sh
@@ -0,0 +1,21 @@
+python run_summarization.py \
+--mode=decode \
+--single_pass=True \
+--data_path=./data/sample250-test.bin \
+--vocab_path=./data/sample250-test.vocab \
+--log_root=logroot \
+--exp_name=test-experiment \
+--max_dec_steps=200 \
+--max_enc_steps=2000 \
+--num_sections=5 \
+--max_section_len=400 \
+--batch_size=4 \
+--vocab_size=50000 \
+--use_do=True \
+--optimizer=adagrad \
+--do_prob=0.25 \
+--hier=True \
+--split_intro=True \
+--fixed_attn=True \
+--legacy_encoder=False \
+--coverage=False
